0d136309ba06aeb7bdb3f3b67aef6c3e7f217483
Daniel <danielaphillion@gmail.com>
2026-05-16 10:56:13 -0400

    feat: func to set remote origin

---
+++ commands/origin.py
@@ -0,0 +1,45 @@
+import exceptions
+import utils
+from pathlib import Path
+import json
+import sys
+
+
+def get_entered_origin() -> str | None:
+    """Retrieve the origin entered by the user."""
+
+    return sys.argv[2] if len(sys.argv) > 2 else None
+
+
+def write_origin_to_config(remote: str = get_entered_origin()) -> None:
+    """Write the origin to the config file."""
+
+    with open(Path(utils.working_directory_path / ".sccs" / "config" / "config.json"), "r+", encoding="utf-8", newline="\n") as f:
+        config = json.load(f)
+        config["remote"] = remote
+        f.seek(0)
+        json.dump(config, f, indent=4)
+        f.truncate()
+
+
+def main():
+    """Run functions for the <sccs clone> command."""
+
+    write_origin_to_config()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+
+    except exceptions.SCCSException as e:
+        print(f"An error occurred:\n{e}\n")
+        sys.exit(1)
+
+    except Exception as e:
+        print(f"An unexpected error occurred:\n\n{type(e).__name__}: {e}\n")
+        sys.exit(2)
+else:
+    raise exceptions.FileImportedAsModuleError(
+        "This file cannot be run as a module. Please run it as a script."
+    )
