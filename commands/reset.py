624e909ffac03c4db951c34ef7447e1bf7dd2285
Daniel <danielaphillion@gmail.com>
2026-05-26 11:14:19 -0400

    feat: print success message

--- commands/reset.py
+++ commands/reset.py
@@ -25,10 +25,19 @@ def reset(cwd: Path | None = None) -> None:
     shutil.copy2(utils.validate_commit(folder="docx", commit=latest_commit), cwd / utils.current_file_docx_path)
 
 
+def print_success_message() -> None:
+    """Print a success message after resetting the document."""
+    print(
+        "All uncommitted changes have been deleted. The document has been reset to the "
+        "latest commit."
+    )
+
+
 def main() -> None:
     """Main function to handle the <reset> command."""
     utils.check_sccs_layout()
     reset()
+    print_success_message()
     
 
 if __name__ == "__main__":
