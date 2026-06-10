cc1c222393b4185935cc96fc40665d5b63f5dc78
Daniel <danielaphillion@gmail.com>
2026-06-10 13:50:59 -0400

    refact: remote unneeded param from push

--- commands/push.py
+++ commands/push.py
@@ -18,17 +18,19 @@ Repository = RepositoryLayout(Path.cwd())
 
 def get_matching_file_paths(
     filename: str,
-    updated_branches: list = None,
     cwd: None | Path = None,
 ) -> list:
     """
     Iterate through 'updated_branches' to retrieve each branch's version of 'filename'.
     """
 
+    updated_branches = utils.get_branch_data(
+            key="updated_branches",
+            file_path=cwd / ".sccs" / "current_branch" / "current_branch.json",
+        ) or []
+
     if cwd is None:
         cwd = Path.cwd()
-    if updated_branches is None:
-        updated_branches = []
     paths = []
     for i in updated_branches:
         branch_dir = cwd / ".sccs" / "branches" / i
@@ -39,11 +41,11 @@ def get_matching_file_paths(
     return paths
 
 
-def push_GET(remote: str) -> requests.Response:
+def push_GET() -> requests.Response:
     """Make a GET request to 'remote'/push, returning the response."""
 
     try:
-        response = requests.get(f"{remote}/push", timeout=60)
+        response = requests.get(f"{utils.get_key_from_config("remote").rstrip("/")}/push", timeout=60)
     except Exception as e:
         raise exceptions.HTTPGetRequestError() from e
 
@@ -63,7 +65,7 @@ def get_repo_objects(cwd: None | Path = None) -> list:
     return objects
 
 
-def compare_hash_lists(remote_objects: list, local_objects: list) -> list:
+def compare_hash_lists(remote_objects: list) -> list:
     """
     Subtract 'remote_objects' from 'local_objects' by converting to sets to get a list
     of objects that remote is missing.
@@ -75,6 +77,8 @@ def compare_hash_lists(remote_objects: list, local_objects: list) -> list:
     Return a list of objects that remote is missing.
     """
 
+    local_objects = get_repo_objects()
+
     obj_to_upload = list(set(local_objects) - set(remote_objects))
     if list(set(remote_objects) - set(local_objects)):
         raise exceptions.MissingCommitObjectsError()
@@ -82,7 +86,7 @@ def compare_hash_lists(remote_objects: list, local_objects: list) -> list:
     return obj_to_upload
 
 
-def zip_files_to_upload(obj_to_upload: list, cwd: None | Path = None) -> io.BytesIO:
+def zip_files_to_upload(remote_objects: list, cwd: None | Path = None) -> io.BytesIO:
     """
     Create a temporary version of the repository with only the files in 'obj_to_upload'
     and metadata files, ensuring that the folder layout is left intact. Compress said
@@ -98,25 +102,18 @@ def zip_files_to_upload(obj_to_upload: list, cwd: None | Path = None) -> io.Byte
 
     document_path = [cwd / f"{cwd.name}.docx"]
 
-    repo_name = cwd.name
-    updated_branches = (
-        utils.get_branch_data(
-            key="updated_branches",
-            file_path=cwd / ".sccs" / "current_branch" / "current_branch.json",
-        )
-        or []
-    )
+    repo_name = cwd.name   
     current_branch_path = [cwd / ".sccs" / "current_branch" / "current_branch.json"]
     commit_msgs_path = [cwd / ".sccs" / "commit_messages" / "commit_messages.json"]
-    obj_to_upload_set = set(obj_to_upload)
+    obj_to_upload_set = set(compare_hash_lists(remote_objects))
     objects_paths = [
         i.resolve()
         for i in (cwd / ".sccs" / "objects").rglob("*")
         if i.is_file() and i.stem in obj_to_upload_set
     ]
 
-    history_paths = get_matching_file_paths("history", updated_branches, cwd)
-    byte_hash_paths = get_matching_file_paths("commit_file_hash", updated_branches, cwd)
+    history_paths = get_matching_file_paths("history", cwd)
+    byte_hash_paths = get_matching_file_paths("commit_file_hash", cwd)
 
     files_to_upload = (
         objects_paths
@@ -158,12 +155,14 @@ def zip_files_to_upload(obj_to_upload: list, cwd: None | Path = None) -> io.Byte
     return buffer
 
 
-def push_POST(remote: str, buffer: io.BytesIO) -> requests.Response:
+def push_POST(buffer: io.BytesIO) -> requests.Response:
     """
     Make a POST request to 'remote', sending 'buffer' as a file.
 
     Return the server response of the POST request to 'remote'.
     """
+
+    remote = utils.get_key_from_config("remote").rstrip("/")
 
     remote_path = urlsplit(remote).path.rstrip("/")
     if not remote_path.endswith(f"/repos/{Path.cwd().name}"):
@@ -224,17 +223,15 @@ def main() -> None:
 
     print(f" Pushing changes to remote repository at {remote}...\n")
 
-    GET_response = push_GET(remote)
+    GET_response = push_GET()
 
     GET_response.raise_for_status()
 
     remote_objects = GET_response.json().get("objects", [])
+    
+    buffer = zip_files_to_upload(remote_objects)
 
-    local_objects = get_repo_objects()
-    obj_to_upload = compare_hash_lists(remote_objects, local_objects)
-    buffer = zip_files_to_upload(obj_to_upload)
-
-    POST_response = push_POST(remote, buffer)
+    POST_response = push_POST(buffer)
 
     print(f"Status code: {POST_response.status_code}\n")
 
