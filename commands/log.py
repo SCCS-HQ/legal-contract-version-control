d0ba765b6d689b6ab23c41419654109fb5780285
Daniel <danielaphillion@gmail.com>
2026-08-07 05:28:17 -0400

    refactor: improve log output formatting for better readability

--- commands/log.py
+++ commands/log.py
@@ -17,12 +17,13 @@ def print_log(c: SCCSConstants, history_data: dict[str, Any]) -> None:
 
     for i in history_data[c.LOG_DICT_KEY]:
         print(
+            c.LOG_SEPARATOR + c.NEWLINE,
+            c.LOG_COMMIT_FILE_LABEL + i[:c.COMMIT_HASH_DISPLAY_LENGTH] + c.NEWLINE,
+            c.LOG_AUTHOR_LABEL + history_data[c.LOG_DICT_KEY][i][c.AUTHOR_DICT_KEY] + c.NEWLINE,
+            c.LOG_DATE_LABEL + history_data[c.LOG_DICT_KEY][i][c.TIMESTAMP_DICT_KEY] + c.NEWLINE,
+            c.LOG_MESSAGE_LABEL + history_data[c.LOG_DICT_KEY][i][c.MESSAGE_DICT_KEY] + c.NEWLINE,
             c.LOG_SEPARATOR,
-            c.LOG_COMMIT_FILE_LABEL + i[:c.COMMIT_HASH_DISPLAY_LENGTH],
-            c.LOG_AUTHOR_LABEL + history_data[c.LOG_DICT_KEY][i][c.AUTHOR_DICT_KEY],
-            c.LOG_DATE_LABEL + history_data[c.LOG_DICT_KEY][i][c.TIMESTAMP_DICT_KEY],
-            c.LOG_MESSAGE_LABEL + history_data[c.LOG_DICT_KEY][i][c.MESSAGE_DICT_KEY],
-            c.LOG_SEPARATOR,
+            sep=""
         )
 
 
